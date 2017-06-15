a2fed2e90a413302f51bad78509efe9f1bc947a1
chrismahcpc <cmah@cpcstrategy.com>
2017-06-15 13:01:15 -0700

    fixed bug for clients with total products as a multiple of 250

--- productstatuses_list.py
+++ productstatuses_list.py
@@ -18,6 +18,7 @@
 
 import sys
 import csv
+import subprocess
 import codecs
 import os
 import json
@@ -43,29 +44,25 @@ def main(argv):
     reader = csv.reader(file)
     rsm_list = list(list(rec) for rec in reader)
   cwd = os.getcwd()
-  gmc_stats = []
+  client = cwd.split('\\')[-1]
   alerts = []
-  for client in os.listdir(cwd):
-    if '.' not in client:
-      print(client)
-      os.chdir(cwd + "\\" + client)
-      os.system('oauth2.py')
-      start = time.time()
-      counts = approvals(argv) 
-      print("Runtime: %s seconds ---" % (time.time() - start))
-      threshold_index = [ind for ind in xrange(len(rsm_list)) if client in rsm_list[ind]]
-      prev_index = [ind for ind in xrange(len(prev_count)) if client in prev_count[ind]]
-      rsm = rsm_list[threshold_index[0]][2]
-      row = [client, rsm, counts[0], counts[1]]
-      if len(prev_index) > 0 and float(prev_count[prev_index[0]][2]) != 0:  
-        prev_approved = prev_count[prev_index[0]][2]
-        change = float(row[2])/float(prev_approved) - 1
-        threshold = rsm_list[threshold_index[0]][3]
-        if change < float(threshold):
-          alert = [rsm_list[threshold_index[0]][0], rsm, prev_approved, counts[0], counts[1], change, threshold]
-          alerts.append(alert)
-      gmc_stats.append(row)
-      os.chdir('../')
+  gmc_stats = []
+  start = time.time()
+  counts = approvals(argv) 
+  print("Runtime: %s seconds ---" % (time.time() - start))
+  threshold_index = [ind for ind in xrange(len(rsm_list)) if client in rsm_list[ind]]
+  prev_index = [ind for ind in xrange(len(prev_count)) if client in prev_count[ind]]
+  rsm = rsm_list[threshold_index[0]][2]
+  row = [client, rsm, counts[0], counts[1]]
+  if len(prev_index) > 0 and float(prev_count[prev_index[0]][2]) != 0:  
+    prev_approved = prev_count[prev_index[0]][2]
+    change = float(row[2])/float(prev_approved) - 1
+    threshold = rsm_list[threshold_index[0]][3]
+    if change < float(threshold):
+      alert = [rsm_list[threshold_index[0]][0], rsm, prev_approved, counts[0], counts[1], change, threshold]
+      alerts.append(alert)
+  gmc_stats.append(row)
+
 
   names = [item[2] for item in rsm_list]
   names.pop(0)
@@ -76,7 +73,7 @@ def main(argv):
     if len(filtered) > 0:
       sendEmail(name, filtered)
 
-  with open('feedstats.csv', 'w') as f:
+  with open('./feedstats.csv', 'w') as f:
     filewriter = csv.writer(f, dialect='excel')
     filewriter.writerow(['Client', 'Products Approved', 'Total Products'])
     filewriter.writerows(gmc_stats)    
@@ -104,7 +101,7 @@ def approvals(argv):
         result = request.execute()
         if shopping_common.json_absent_or_false(result, 'resources'):
           print 'No products were found.'
-          return [0,0]
+          return [approvals, total]
         else:
           products = result['resources']
           for product in products:
@@ -120,93 +117,3 @@ def approvals(argv):
   except client.AccessTokenRefreshError:
     print ('The credentials have been revoked or expired, please re-run the '
            'application to re-authorize')
-
-def sendEmail(rsm, filtered):
-  email = find_email(rsm)
-  msg = MIMEMultipart('alternative')
-  msg['Subject'] = "URGENT: GMC Feed Status Alert"
-  msg['From'] = "Feed Monitor"
-  msg['To'] = email
-  html = build_email(filtered)
-  part1 = MIMEText(html, 'html')
-  msg.attach(part1)
-  server = smtplib.SMTP('smtp.gmail.com', 587)
-  server.ehlo()
-  server.starttls()
-  server.login("cpcstrategy99@gmail.com", "cpcdiego87")
-  server.sendmail('cpcstrategy99@gmail.com', ['cmah@cpcstrategy.com'], msg.as_string())
-  server.quit()
-
-def build_email(filtered):
-  html = ''
-  html = html +'<h2 style=\'margin-bottom:0px;font-family:arial\'><u>Feed Statuses - %s</u></h2>' % filtered[0][1]
-  html = html + '<table style=\'border-spacing:10px;font-family:arial\'><tr style=\'font-weight:bold\'> <td>Client</td> <td>Previous # Valid</td> <td>Current # Valid</td> <td>Current Total Items</td> <td>% Change</td> <td>Threshold</td></tr>'
-  for client in filtered:
-    percent = str(int(round(client[5] * 100))) + '%'
-    threshold = int(float(client[6]) * 100)
-    threshold = str(threshold) + '%'
-    html = html + '<tr><td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td></tr>' % (client[0], client[2], client[3], client[4], percent, threshold)
-  html = html + '</table>'
-  return html
-
-def find_email(name):
-
-  if name == "Adam":
-    return "adam@cpcstrategy.com"
-  elif name == "Stephen K":
-    return "stephen@cpcstrategy.com"
-  # elif name == "Stephen M":
-  #   return "smessana@cpcstrategy.com"
-  elif name == "Will":
-    return "william@cpcstrategy.com"
-  # elif name == "Jason":
-  #   return "jason@cpcstrategy.com"
-  # elif name == "Jostin":
-  #   return "Jostin@cpcstrategy.com"
-  # elif name == "Chris":
-  #   return "chris@cpcstrategy.com"
-  # elif name == "Josh":
-  #   return "josh@cpcstrategy.com"
-  elif name == "Roman":
-    return "roman@cpcstrategy.com"
-  # elif name == "Anthony":
-  #   return "anthony@cpcstrategy.com"
-  # elif name == "Lewis":
-  #   return "lewis@cpcstrategy.com"
-  elif name == "La Broi":
-    return "labroi@cpcstrategy.com"
-  # elif name == "Erick":
-  #   return "erick@cpcstrategy.com"
-  # elif name == "Meghan":
-  #   return "meghan@cpcstrategy.com"
-  # elif name == "Caroline":
-  #   return "caroline@cpcstrategy.com"
-  # elif name == "Ben":
-  #   return "ben@cpcstrategy.com"
-  # elif name == "Michael":
-  #   return "michael@cpcstrategy.com"
-  # elif name == "Dean":
-  #   return "dean@cpcstrategy.com"
-  # elif name == "Nick":
-  #   return "nmanessis@cpcstrategy.com"
-  # elif name == "Emiri":
-  #   return "emiri@cpcstrategy.com"
-  # elif name == "Dianne":
-  #   return "dianne@cpcstrategy.com"
-  # elif name == "Helen":
-  #   return "helen@cpcstrategy.com"
-  elif name == "Keith":
-    return "keith@cpcstrategy.com"
-  # elif name == "Sam":
-  #   return "sam@cpcstrategy.com"
-  # elif name == "Melissa":
-  #   return "melissa@cpcstrategy.com"
-  elif name == "Eliza":
-    return "eliza@cpcstrategy.com"
-  elif name == "Alia":
-    return "alia@cpcstrategy.com"  
-  else:
-    return "sb@cpcstrategy.com"
-
-if __name__ == '__main__':
-  main(sys.argv)
